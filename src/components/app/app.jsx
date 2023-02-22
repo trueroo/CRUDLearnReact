8197612c52c1230ba11249e253068bf35ac5ba74
Roo Voterpolov <voterpolov@gmail.com>
2023-02-22 02:32:45 +0100

    add imitation get request to server

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,6 +7,12 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 const App = () => {
+	const data = [
+		{ name: 'John Doe', salary: 4000, increase: false, id: 1 },
+		{ name: 'John Smith', salary: 5800, increase: false, id: 2 },
+		{ name: 'Boris Brejcha', salary: 3100, increase: true, id: 3 },
+	];
+
 	return (
 		<div className="app">
 			<AppInfo />
@@ -16,7 +22,7 @@ const App = () => {
 				<AppFilter />
 			</div>
 
-			<EmployeesList />
+			<EmployeesList data={data} />
 			<EmployeesAddForm />
 		</div>
 	);
--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -1,23 +1,19 @@
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css';
 
-const EmployeesList = () => {
-	return (
-		<ul className="app-list list-group">
+const EmployeesList = ({ data }) => {
+	const employees = data.map((el) => {
+		const { id, ...elProps } = el;
+
+		return (
 			<EmployeesListItem
-				name="John Doe"
-				salary={1800}
+				key={id}
+				{...elProps}
 			/>
-			<EmployeesListItem
-				name="John Smith"
-				salary={4300}
-			/>
-			<EmployeesListItem
-				name="Boris Brejcha"
-				salary={3000}
-			/>
-		</ul>
-	);
+		);
+	});
+
+	return <ul className="app-list list-group">{employees}</ul>;
 };
 
 export default EmployeesList;
