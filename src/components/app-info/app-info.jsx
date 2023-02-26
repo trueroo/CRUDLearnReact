7be00c8ce2e8c04b974b3a1bcbd6cb6045e8c76b
Roo Voterpolov <voterpolov@gmail.com>
2023-02-26 20:48:30 +0100

    add new methods for buttons

--- src/components/app-info/app-info.jsx
+++ src/components/app-info/app-info.jsx
@@ -1,11 +1,11 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({ employees, increased }) => {
 	return (
 		<div className="app-info">
 			<h1>Employees accounting in 'Kekw LLC'</h1>
-			<h2>Employees count: </h2>
-			<h2>Bonus: </h2>
+			<h2>Employees count: {employees}</h2>
+			<h2>Bonus: {increased}</h2>
 		</div>
 	);
 };
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -13,9 +13,9 @@ class App extends Component {
 		super(props);
 		this.state = {
 			data: [
-				{ name: 'Sarah Kerrigan', salary: 4800, increase: false, id: 1 },
-				{ name: 'Jim Raynor', salary: 3000, increase: true, id: 2 },
-				{ name: 'Homer Simpson', salary: 2000, increase: false, id: 3 },
+				{ name: 'Sarah Kerrigan', salary: 4800, increase: false, rise: true, id: 1 },
+				{ name: 'Jim Raynor', salary: 3000, increase: true, rise: false, id: 2 },
+				{ name: 'Homer Simpson', salary: 2000, increase: false, rise: false, id: 3 },
 			],
 		};
 		this.maxId = 4;
@@ -35,6 +35,7 @@ class App extends Component {
 			name,
 			salary,
 			increase: false,
+			rise: false,
 			id: this.maxId++,
 		};
 		this.setState(({ data }) => {
@@ -45,10 +46,26 @@ class App extends Component {
 		});
 	};
 
+	onToggleProp = (id, prop) => {
+		this.setState(({ data }) => ({
+			data: data.map((item) => {
+				if (item.id === id) {
+					return { ...item, [prop]: !item[prop] };
+				}
+				return item;
+			}),
+		}));
+	};
+
 	render() {
+		const employees = this.state.data.length;
+		const increased = this.state.data.filter((item) => item.increase).length;
 		return (
 			<div className="app">
-				<AppInfo />
+				<AppInfo
+					employees={employees}
+					increased={increased}
+				/>
 
 				<div className="search-panel">
 					<SearchPanel />
@@ -58,6 +75,7 @@ class App extends Component {
 				<EmployeesList
 					data={this.state.data}
 					onDelete={this.deleteItem}
+					onToggleProp={this.onToggleProp}
 				/>
 				<EmployeesAddForm onAdd={this.addItem} />
 			</div>
